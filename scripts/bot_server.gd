827693e35160be853fca19750ae45804974baa0e
Manglemix <shabouza030@gmail.com>
2022-04-18 21:43:15 -0600

    added rosout

--- scripts/bot_server.gd
+++ scripts/bot_server.gd
@@ -10,16 +10,21 @@ enum {
 	MAKE_AUTONOMOUS,
 	MAKE_MANUAL,
 	ECHO,
-	MANUAL_HOME			# Calibrates motors
+	MANUAL_HOME,			# Calibrates motors
+	CONNECTED,
+	PING,
+	ROSOUT,					# Rosout msg from bot, should be a security level and a ready-to-print string
 }
 
 signal manual_home_complete
-signal odometry_received(odom)
-signal arm_angle_received(angle)
+signal odometry(odometry)
+signal arm_angle(angle)
+signal autonomy_changed
+signal rosout(level, msg)
 signal packet_received(delta)
 
 const DEADZONE := 0.1
-const BROADCAST_DELAY := 1.0
+const BROADCAST_DELAY := 0.5
 const INPUT_RATE := 20
 
 var bot_tcp_server := TCP_Server.new()
@@ -29,22 +34,32 @@ var bot_udp := PacketPeerUDP.new()
 
 var broadcaster := PacketPeerUDP.new()
 
-var _manually_homing := false
-var _last_packet_time := OS.get_system_time_msecs()
 var start_time := OS.get_system_time_secs()
 var broadcasting := false
 var listening := false
 var bind_addr: String
 var bind_port: int
-var broadcast_timer := Timer.new()
-var _input_timer := 0.0
+
+var _manually_homing := false
+var _last_packet_time := OS.get_system_time_msecs()
+var _broadcast_timer := Timer.new()
+var _is_autonomous := true
+var _input_timer := Timer.new()
+
+
+func get_is_autonomous() -> bool:
+	return _is_autonomous
 
 
 func _ready():
 	set_process_input(false)
 	set_process(false)
-	add_child(broadcast_timer)
-	broadcast_timer.connect("timeout", self, "broadcast")
+	add_child(_broadcast_timer)
+	# warning-ignore:return_value_discarded
+	_broadcast_timer.connect("timeout", self, "broadcast")
+	add_child(_input_timer)
+	_input_timer.one_shot = true
+	_input_timer.wait_time = 1.0 / INPUT_RATE
 
 
 func start_brodcasting(addr: String, port: int) -> int:
@@ -70,7 +85,7 @@ func start_brodcasting(addr: String, port: int) -> int:
 		return err
 	
 	broadcasting = true
-	broadcast_timer.start(BROADCAST_DELAY)
+	_broadcast_timer.start(BROADCAST_DELAY)
 	return OK 
 
 
@@ -101,19 +116,22 @@ func reset_connection():
 	listening = false
 	set_process(false)
 	set_process_input(false)
-	broadcast_timer.stop()
+	_broadcast_timer.stop()
 
 
 func broadcast():
+	# warning-ignore:return_value_discarded
 	broadcaster.put_packet((bind_addr + ":" + str(bind_port)).to_utf8())
 
 
 func make_autonomous():
+	# warning-ignore:return_value_discarded
 	bot_tcp.put_data(_make_byte(MAKE_AUTONOMOUS))
 	push_warning("Sent MAKE_AUTONOMOUS to bot")
 
 
 func make_manual():
+	# warning-ignore:return_value_discarded
 	bot_tcp.put_data(_make_byte(MAKE_MANUAL))
 	push_warning("Sent MAKE_MANUAL to bot")
 
@@ -123,6 +141,7 @@ func manual_home(idx: int):
 	var data := _make_byte(MANUAL_HOME)
 	assert(idx >= 0 and idx < 256)
 	data.append(idx)
+	# warning-ignore:return_value_discarded
 	bot_tcp.put_data(data)
 	push_warning("Sent MANUAL_HOME to bot")
 
@@ -130,20 +149,19 @@ func manual_home(idx: int):
 func _input(event):
 	if (event is InputEventJoypadMotion and abs(event.axis_value) >= DEADZONE) or \
 		event is InputEventJoypadButton:
-		if event is InputEventJoypadMotion and _input_timer > 0: return
-		_input_timer = 1.0 / INPUT_RATE
+		if event is InputEventJoypadMotion and not _input_timer.is_stopped(): return
+		_input_timer.start()
 		if event.is_action_pressed("end_manual_home"): emit_signal("manual_home_complete")
 		
 		var msg := _get_controller_state()
+		# warning-ignore:return_value_discarded
 		msg.insert(0, JOY_INPUT)
 		var err := bot_udp.put_packet(msg)
 		if err != OK:
 			push_error("Faced error code " + str(err) + " while sending input data!")
 
 
-func _process(delta):
-	if _input_timer > 0:
-		_input_timer -= delta
+func _process(_delta):
 	match bot_udp.get_available_packet_count():
 		0: pass
 		-1:
@@ -156,11 +174,16 @@ func _process(delta):
 			bot_tcp.set_no_delay(true)
 			if broadcasting:
 				broadcasting = false
-				broadcast_timer.stop()
+				_broadcast_timer.stop()
 		return
 	
 	match bot_tcp.get_available_bytes():
-		0: pass
+		0:
+			if bot_tcp.get_status() != StreamPeerTCP.STATUS_CONNECTED:
+				push_error("Lost connection to lunabot!")
+				# warning-ignore:return_value_discarded
+				get_tree().change_scene("res://scenes/bind_addr.tscn")
+				reset_connection()
 		-1:
 			push_error("Got -1")
 		_: _poll_tcp()
@@ -176,7 +199,7 @@ func _poll_udp():
 	emit_signal("packet_received", current_time - _last_packet_time)
 	_last_packet_time = current_time
 	
-	if msg.get_string_from_utf8() == "hello":
+	if msg[0] == CONNECTED:
 		set_process_input(true)
 		var err := bot_udp.set_dest_address(bot_udp.get_packet_ip(), bot_udp.get_packet_port())
 		if broadcasting:
@@ -184,18 +207,13 @@ func _poll_udp():
 				push_error("Error code: " + str(err) + " while trying to connect to Lunabot_udp")
 				return
 			broadcasting = false
-			broadcast_timer.stop()
+			_broadcast_timer.stop()
+		return
 	
 	_handle_message(msg)
 
 
 func _poll_tcp():
-	if not bot_tcp.is_connected_to_host():
-		push_error("Lost connection to lunabot!")
-		get_tree().change_scene("res://scenes/bind_addr.tscn")
-		reset_connection()
-		return
-	
 	var result := bot_tcp.get_data(bot_tcp.get_available_bytes())
 	
 	if result[0] != OK:
@@ -223,22 +241,39 @@ func _handle_message(msg: PoolByteArray):
 			push_warning("bot_udp has requested to terminate")
 		ODOMETRY:
 			# Pass data to rust module to deserialize
-			emit_signal("odometry_received", Odometry.new(
+			emit_signal("odometry", Odometry.new(
 				Serde.deserialize_quat(msg.subarray(0, 15)),
 				Serde.deserialize_vector3(msg.subarray(16, 27)),
 				Serde.deserialize_vector3(msg.subarray(28, 39)),
 				Serde.deserialize_quat(msg.subarray(40, 55))
 			))
 		ARM_ANGLE:
-			emit_signal("arm_angle_received", Serde.deserialize_f32(msg))
+			emit_signal("arm_angle", Serde.deserialize_f32(msg))
 		ECHO:
 			match msg[0]:
 				MAKE_AUTONOMOUS:
+					if _is_autonomous:
+						push_warning("Bot echoed MAKE_AUTONOMOUS but we already know it is...")
+						return
+					_is_autonomous = true
+					emit_signal("autonomy_changed")
 					push_warning("Bot is autonomous")
 				MAKE_MANUAL:
+					if not _is_autonomous:
+						push_warning("Bot echoed MAKE_MANUAL but we already know it is...")
+						return
+					_is_autonomous = false
+					emit_signal("autonomy_changed")
 					push_warning("Bot is manual")
+				_:
+					push_error("Unrecognized echo header: " + str(msg[0]))
+		ROSOUT:
+			var level := msg[0]
+			msg.remove(0)
+			var txt := msg.get_string_from_utf8()
+			emit_signal("rosout", level, txt)
 		_:
-			push_error("Received unrecognized header: " + str(msg[0]))
+			push_error("Received unrecognized header: " + str(header))
 
 
 func _get_joy_axis(device: int, axis: int) -> float:
@@ -256,8 +291,8 @@ func _get_controller_state() -> PoolByteArray:
 		Serde.serialize_f32(_get_joy_axis(0, JOY_AXIS_6)),
 		Serde.serialize_f32(_get_joy_axis(0, JOY_AXIS_7)),
 		Serde.serialize_f32(_get_joy_axis(0, JOY_AXIS_3)),
-		int(Input.is_joy_button_pressed(0, JOY_DPAD_RIGHT)) - int(Input.is_joy_button_pressed(0, JOY_DPAD_LEFT)),
-		int(Input.is_joy_button_pressed(0, JOY_DPAD_UP)) - int(Input.is_joy_button_pressed(0, JOY_DPAD_DOWN)),
+		Serde.serialize_f32(float(Input.is_joy_button_pressed(0, JOY_DPAD_RIGHT)) - float(Input.is_joy_button_pressed(0, JOY_DPAD_LEFT))),
+		Serde.serialize_f32(float(Input.is_joy_button_pressed(0, JOY_DPAD_UP)) - float(Input.is_joy_button_pressed(0, JOY_DPAD_DOWN))),
 		Serde.serialize_bool_array([
 			Input.is_joy_button_pressed(0, JOY_SONY_SQUARE),
 			Input.is_joy_button_pressed(0, JOY_SONY_X),
